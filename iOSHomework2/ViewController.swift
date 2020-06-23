7a26c7970afbb496d5bd647e3b3be32aa3043bb5
92-BadriyaMousaAlajmi <Badora2520@gmail.com>
2020-06-23 04:22:22 +0300

    finally doneeeee !!

--- iOSHomework2/ViewController.swift
+++ iOSHomework2/ViewController.swift
@@ -9,16 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var membersNamesArray  : [String] = [""]
+    var membersNamesArray  : [String] = []
     
-    var convertToLetter = true
+    var convertToLetter = false
     @IBOutlet weak var secretSocietyNameLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
     @IBAction func addMember(_ sender: Any) {
         
-        let member = nameTextField.text!
+       let member = nameTextField.text!
         
         // MARK: -   1️⃣ تحت الخط membersNamesArray إلي المصفوفه memberقم بإضافة المتغير
+        membersNamesArray.append(member)
         
         
         
@@ -33,8 +34,8 @@ class ViewController: UIViewController {
     @IBAction func letterButton(_ sender: Any) {
         
         // MARK: -  4️⃣ functionCall داخل المتغير  secretNameLetter قم باستدعاء الدالة
-        
-        var functionCall = ""
+        let functionCall = secretNameLetter(members: membersNamesArray)
+        //(member: membersNamesArray)
         
         
         // MARK: -   النهاية
@@ -52,7 +53,7 @@ class ViewController: UIViewController {
         
         // MARK: -  5️⃣ functionCall داخل المتغير  secretNameEmoji قم باستدعاء الدالة
         
-        var functionCall = ""
+        let functionCall = secretNameEmoji(membersNamesArray: membersNamesArray)
         
         // MARK: -   النهاية
         
@@ -66,6 +67,12 @@ class ViewController: UIViewController {
     
     // MARK: - 2️⃣ تحت هذا الخط secretNameLetter قم بكتابة الداله
     
+    func secretNameLetter(members: [String])->String {
+        var secret = ""
+        for member in members {
+            secret += member.prefix(1)}
+        return secret
+    }
     
     // MARK: -   النهاية
     
@@ -74,11 +81,50 @@ class ViewController: UIViewController {
     
     
     // MARK: - 3️⃣ تحت هذا الخط secretNameEmoji قم بكتابة الداله
+    func secretNameEmoji(membersNamesArray: [String])->String{
+        var secret = ""
+        let emojiLetter: [String:String]=[
+        "A" : "🍻",
+        "B" : "🔥",
+        "C" : "🌈",
+        "D" : "😡",
+        "E" : "👿",
+        "F" : "🐶",
+        "G" : "💝",
+        "H" : "😍",
+        "I" : "😉",
+        "J" : "😳",
+        "K" : "🌟",
+        "L" : "🌺",
+        "M" : "🎶",
+        "N" : "🐵",
+        "O" : "🍓",
+        "P" : "💜",
+        "Q" : "😜",
+        "R" : "😵",
+        "S" : "💃",
+        "T" : "💎",
+        "U" : "⛄",
+        "V" : "🌊",
+        "W" : "💰",
+        "X" : "🐰",
+        "Y" : "🍉",
+        "Z" : "💛"]
+        
+    for member in membersNamesArray{
+    let prefix = String(member.prefix(1)).uppercased()
+    let emoji = emojiLetter[prefix] ?? "!?"
+     secret += emoji
+         }
+         
+     return secret
+
     
+    }
     
     // MARK: -   النهاية
-    
-    
-    
+func encryptWith(emoji : Bool, array: [String])-> String {
+if emoji {return secretNameEmoji(membersNamesArray: array)}
+else{return secretNameLetter(members: array)}
 }
-
+}
